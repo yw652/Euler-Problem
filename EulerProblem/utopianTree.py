e72adaf84d57dfe7177158032966957c3595c6a7
Yitong Wang <yw652@nyu.edu>
2015-01-24 09:42:32 +0800

    Update description
    https://www.hackerrank.com/challenges/utopian-tree
    problem description

--- EulerProblem/utopianTree.py
+++ EulerProblem/utopianTree.py
@@ -1,3 +1,10 @@
+'''
+The Utopian tree goes through 2 cycles of growth every year. The first growth cycle occurs during the spring, 
+when it doubles in height. The second growth cycle occurs during the summer, when its height increases by 1 meter. 
+Now, a new Utopian tree sapling is planted at the onset of the spring. Its height is 1 meter. 
+Can you find the height of the tree after N growth cycles?
+'''
+
 import sys
 import math
 
