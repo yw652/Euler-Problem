58fef182a42bf59a17a09236964b0521d61a1792
Yitong Wang <yw652@nyu.edu>
2015-01-24 09:48:54 +0800

    Update comment

--- EulerProblem/complement.py
+++ EulerProblem/complement.py
@@ -1,18 +1,27 @@
 __author__ = 'yitongwang'
 import math
 import numpy as np
-
+'''
+Find the binary complement of user's choice of integer
+'''
 
 def complement():
-    n = int(raw_input())
+    n = int(raw_input("Enter your choice of integer for binary's complement calculation"))
     list = []
+    
+    #Decomposing the number to find its binary expression
     while(n != 0):
         list.append(n % 2)
         n /= 2
+    
     list = list[::-1]
     binary = []
+    
+    #Create its binary complement
     for i in list:
         binary.append(1-i)
+        
+    #Calculate the arithmetic form of the complement
     sum = 0
     for i in range(0, len(binary)):
         sum += math.pow(2,len(binary)-1-i) * binary[i]
